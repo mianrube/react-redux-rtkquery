ac16a30a5373d25dda3229597bd130ec5ee870c1
Miguel Ángel Ruiz Begines <mianrube@outlook.com>
2023-11-26 20:12:50 +0100

    BaseQuery configuration with bearer token interceptor example

--- src/store/todos/todosApi.ts
+++ src/store/todos/todosApi.ts
@@ -1,6 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Todo } from '../../types/interfaces';
 
+// baseQuery configuration with bearer token interceptor
+// const baseQuery = fetchBaseQuery({
+//   baseUrl: 'https://localhost:7157/api/v1.0',
+//   prepareHeaders: (headers) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       headers.set('authorization', `Bearer ${token}`);
+//     }
+//     return headers;
+//   },
+// });
+
 export const todosApi = createApi({
   reducerPath: 'todosApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://localhost:7157/api/v1.0' }),
